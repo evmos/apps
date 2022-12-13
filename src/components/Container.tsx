a7180ad6849b11e9e23cb1f7ff5302db2fc495e3
miliPaoletti <milagros.paoletti@gmail.com>
2022-12-13 16:34:25 +0100

    test

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,11 @@
-
-const Container = ({children}: {children: JSX.Element}) => {
-    return (
-     <div className='bg-black grid min-h-screen grid-template-[120px]'>
-    <div className='w-full h-full bg-[url("/stars.svg")] bg-repeat bg-auto bg-bottom ' >
+const Container = ({ children }: { children: JSX.Element }) => {
+  return (
+    <div className="bg-black grid min-h-screen grid-template-[120px]">
+      <div className='w-full h-full bg-[url("/stars.svg")] bg-repeat bg-auto bg-bottom'>
         {children}
+      </div>
     </div>
-    </div>)
-}
+  );
+};
 
-export default Container
+export default Container;

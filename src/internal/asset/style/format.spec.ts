2ab4876e82ebca57a4a3cc090ca72aea82ff1971
miliPaoletti <milagros.paoletti@gmail.com>
2022-12-22 16:30:01 +0100

    fix test

--- src/internal/asset/style/format.spec.ts
+++ src/internal/asset/style/format.spec.ts
@@ -1,10 +1,15 @@
+import { BigNumber } from "ethers";
 import { getReservedForFeeText } from "./format";
 
 describe("Test Styles for Asset", () => {
   it("getReserverdForFee function", () => {
-    const msg = getReservedForFeeText(5, "EVMOS", "EVMOS");
+    const msg = getReservedForFeeText(
+      BigNumber.from("300000000000000000"),
+      "EVMOS",
+      "EVMOS"
+    );
     expect(msg).toBe(
-      "5 EVMOS is reserved for transaction fees on the EVMOS network."
+      "0.3 EVMOS is reserved for transaction fees on the EVMOS network."
     );
   });
 });

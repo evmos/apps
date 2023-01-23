47c5aad62e191bba5253717fb586db0e45e073ea
miliPaoletti <milagros.paoletti@gmail.com>
2023-01-23 11:08:17 +0100

    add hover for back to mission control

--- src/components/asset/table/AssetsTable.tsx
+++ src/components/asset/table/AssetsTable.tsx
@@ -122,7 +122,7 @@ const AssetsTable = () => {
     <>
       <Link
         href="https://app.evmos.org/"
-        className="text-white flex items-center space-x-3 mb-2 font-bold mx-5 xl:mx-0 justify-center xl:justify-start"
+        className="text-white flex items-center space-x-3 mb-2 font-bold mx-5 xl:mx-0 justify-center xl:justify-start hover:opacity-80"
       >
         <LeftArrowIcon width={15} height={15} />
         <p>Back to Mission Control</p>

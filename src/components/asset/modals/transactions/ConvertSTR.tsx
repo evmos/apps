da6719cb1260b2cbb88b1b40e37a928e9fbeed49
miliPaoletti <milagros.paoletti@gmail.com>
2023-02-09 10:15:49 +0100

    fix: show correct symbol on convert STR

--- src/components/asset/modals/transactions/ConvertSTR.tsx
+++ src/components/asset/modals/transactions/ConvertSTR.tsx
@@ -64,7 +64,7 @@ export const ConvertSTR = ({
 
   return (
     <>
-      <ModalTitle title="Convert WEVMOS" />
+      <ModalTitle title={`Convert ${symbolFrom}`} />
       <div className="text-darkGray3 space-y-3">
         <ContainerModal>
           <>

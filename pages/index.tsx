fe2f69f492ab5d29deeb69e3b2e9089351f87482
miliPaoletti <milagros.paoletti@gmail.com>
2023-02-22 10:31:57 +0100

    test with image in src

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Snackbars = dynamic(
   () => import("../src/components/notification/Snackbars")
 );
 const Footer = dynamic(() => import("../src/components/footer/Footer"));
-
+import twitterPreviewImage from "../src/images/social_share.jpg";
 export default function Home() {
   const queryClient = new QueryClient();
   return (
@@ -50,9 +50,9 @@ export default function Home() {
               {/* <!--  Essential META Tags --> */}
               <meta property="og:title" content="Evmos Assets" />
               <meta property="og:type" content="article" />
-              <meta property="og:image" content="/assets/social_share.jpg" />
+              <meta property="og:image" content={twitterPreviewImage.src} />
               <meta property="og:url" content="https://app.evmos.org/assets" />
-              <meta name="twitter:card" content="/assets/social_share.jpg" />
+              <meta name="twitter:card" content="summary_large_image" />
 
               {/* <!--  Non-Essential, But Recommended --> */}
               <meta

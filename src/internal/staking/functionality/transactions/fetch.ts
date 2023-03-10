03a2ec574639fc62b8445fc3e5337095126d6f53
miliPaoletti <milagros.paoletti@gmail.com>
2023-03-10 10:17:48 +0100

    feat: add undelegate call using undelegate endpoint

--- src/internal/staking/functionality/transactions/fetch.ts
+++ src/internal/staking/functionality/transactions/fetch.ts
@@ -47,3 +47,49 @@ export async function delegateBackendCall(
     };
   }
 }
+
+export async function undelegateBackendCall(
+  pubkey: string,
+  address: string,
+  valAddress: string,
+  amount: string
+): Promise<{
+  error: boolean;
+  message: string;
+  data: IBCTransferResponse | null;
+}> {
+  try {
+    const post = await fetch(`${EVMOS_BACKEND}/undelegate`, {
+      method: "post",
+      body: JSON.stringify({
+        transaction: {
+          pubKey: pubkey,
+          sender: address,
+          gas: 0,
+        },
+        message: {
+          amount: amount,
+          validatorAddress: valAddress,
+        },
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = (await post.json()) as IBCTransferResponse;
+    if ("error" in data) {
+      // TODO: add sentry call here!
+      return {
+        error: true,
+        message: GENERATING_TX_NOTIFICATIONS.ErrorGeneratingTx,
+        data: null,
+      };
+    }
+    return { error: false, message: "", data: data };
+  } catch (e) {
+    // TODO: add sentry call here!
+    return {
+      error: true,
+      message: GENERATING_TX_NOTIFICATIONS.ErrorGeneratingTx,
+      data: null,
+    };
+  }
+}

a430de64507fb8ef002844b73b5efb6881a6d2af
Milagros Paoletti <40247040+miliPaoletti@users.noreply.github.com>
2023-03-15 14:31:25 +0100

    feat: add claim rewards on click (#154)

--- src/internal/staking/functionality/transactions/fetch.ts
+++ src/internal/staking/functionality/transactions/fetch.ts
@@ -141,3 +141,43 @@ export async function redelegateBackendCall(
     };
   }
 }
+
+export async function rewardsBackendCall(
+  pubkey: string,
+  address: string
+): Promise<{
+  error: boolean;
+  message: string;
+  data: IBCTransferResponse | null;
+}> {
+  try {
+    const post = await fetch(`${EVMOS_BACKEND}/rewards`, {
+      method: "post",
+      body: JSON.stringify({
+        transaction: {
+          pubKey: pubkey,
+          sender: address,
+          gas: 0,
+        },
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = (await post.json()) as IBCTransferResponse;
+    if ("error" in data) {
+      // TODO: add sentry call here!
+      return {
+        error: true,
+        message: GENERATING_TX_NOTIFICATIONS.ErrorGeneratingTx,
+        data: null,
+      };
+    }
+    return { error: false, message: "", data: data };
+  } catch (e) {
+    // TODO: add sentry call here!
+    return {
+      error: true,
+      message: GENERATING_TX_NOTIFICATIONS.ErrorGeneratingTx,
+      data: null,
+    };
+  }
+}

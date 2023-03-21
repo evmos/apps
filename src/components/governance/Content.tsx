d5aa9c4242838abf5940945e541622c152b9b510
Milagros Paoletti <40247040+miliPaoletti@users.noreply.github.com>
2023-03-21 15:22:49 +0100

    feat: add commonwealth banner (#158)

--- src/components/governance/Content.tsx
+++ src/components/governance/Content.tsx
@@ -1,9 +1,19 @@
+import { COMMONWEALTH_URL } from "../../internal/common/links";
+import BannerBlack from "../common/banners/BannerBlack";
 import ContainerProposals from "./proposals/ContainerProposals";
 
 const Content = () => {
   return (
-    <div className="mt-5 overflow-y-auto max-h-[65vh] xl:scrollbar-hide text-white font-[IBM] w-full">
-      <ContainerProposals />
+    <div>
+      <BannerBlack
+        text="Have you ever wondered where proposals come from? Join us in our open
+          and lively discussions over at Commonwealth"
+        href={COMMONWEALTH_URL}
+      />
+
+      <div className="mt-5 overflow-y-auto max-h-[43vh] md:max-h-[55vh] xl:scrollbar-hide text-white font-[IBM] w-full">
+        <ContainerProposals />
+      </div>
     </div>
   );
 };

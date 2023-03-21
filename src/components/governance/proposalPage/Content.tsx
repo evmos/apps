191e0a3bf17c7f29822658a7cab6d4b567dbad91
miliPaoletti <milagros.paoletti@gmail.com>
2023-03-21 16:12:26 +0100

    create new component for proposal description and add loading and error messages

--- src/components/governance/proposalPage/Content.tsx
+++ src/components/governance/proposalPage/Content.tsx
@@ -1,72 +1,20 @@
 import { useRouter } from "next/router";
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import remarkGfm from "remark-gfm";
 import { useProposals } from "../../../internal/governance/functionality/hooks/useProposal";
-import IdContainer from "../common/IdContainer";
-import TitleContainer from "../common/TItleContainer";
-import DescriptionItem from "./DescriptionItem";
+import ProposalDescription from "./ProposalDescription";
 
 const Content = () => {
   const router = useRouter();
 
   const { pid } = router.query;
-  const { proposalDetail } = useProposals(pid as string);
+  const { proposalDetail, loading, error } = useProposals(pid as string);
   return (
     <div className="mt-5 overflow-y-auto max-h-[65vh] md:max-h-[65vh] xl:scrollbar-hide text-white font-[IBM] w-full">
       {/* TODO: create component for container general of cards */}
-      <section
-        className="space-y-5
-    mx-5 xl:mx-0 mb-5 bg-darkGray2 p-5 rounded-2xl font-[IBM] text-sm px-5 text-white "
-      >
-        <div className="flex space-x-4 items-center ">
-          <IdContainer id={proposalDetail.id} />
-          <TitleContainer title={proposalDetail.title} />
-        </div>
-        <div className="space-y-4">
-          <DescriptionItem
-            title="Total Deposit"
-            description={`${proposalDetail.totalDeposit} EVMOS`}
-          />
-          <DescriptionItem
-            title="Voting Start"
-            description={proposalDetail.votingStartTime}
-          />
-          <DescriptionItem
-            title="Voting end"
-            description={proposalDetail.votingEndTime}
-          />
-          <DescriptionItem title="Type" description={proposalDetail.type} />
-          <DescriptionItem
-            title="Submit Time"
-            description={proposalDetail.submitTime}
-          />
-          <DescriptionItem
-            title="Deposit end time"
-            description={proposalDetail.depositEndTime}
-          />
-          <DescriptionItem
-            title="Quorum"
-            description={`${proposalDetail.tallying.quorum} %`}
-          />
-          <DescriptionItem
-            title="Threshold"
-            description={`${proposalDetail.tallying.threshold} %`}
-          />
-          <DescriptionItem
-            title="Veto threshold"
-            description={`${proposalDetail.tallying.vetoThreshold} %`}
-          />
-        </div>
-      </section>
-
-      <section
-        className="markdown space-y-5
-    mx-5 xl:mx-0 mb-5 bg-darkGray2 p-5 rounded-2xl font-[IBM] text-sm px-5 text-white break-words"
-      >
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-          {proposalDetail.description}
-        </ReactMarkdown>
-      </section>
+      <ProposalDescription
+        proposalDetail={proposalDetail}
+        loading={loading}
+        error={error}
+      />
     </div>
   );
 };

bcebce250c07e6090c2546150111e24d5d7198b1
miliPaoletti <milagros.paoletti@gmail.com>
2023-03-24 13:01:33 +0100

    feat: update navToMissionControl component

--- src/components/governance/Content.tsx
+++ src/components/governance/Content.tsx
@@ -2,7 +2,13 @@ import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { COMMONWEALTH_URL } from "../../internal/common/links";
 import { useProposals } from "../../internal/governance/functionality/hooks/useProposals";
+import {
+  EVMOS_PAGE_URL,
+  NAV_TO_GOVERNANCE,
+  NAV_TO_MISSION_CONTROL,
+} from "../common/constants";
 import NavToMissionControl from "../common/navigation/NavToMissionControl";
+import { GOVERNANCE_PATH } from "./common/helpers";
 
 const BannerBlack = dynamic(() => import("../common/banners/BannerBlack"));
 const ContainerProposals = dynamic(
@@ -17,10 +23,16 @@ const Content = () => {
   const { proposals, loading, error, proposalDetail } = useProposals(
     id !== undefined ? (id as string) : ""
   );
+  let href = EVMOS_PAGE_URL;
+  let text = NAV_TO_MISSION_CONTROL;
+  if (id !== undefined) {
+    href = EVMOS_PAGE_URL + GOVERNANCE_PATH;
+    text = NAV_TO_GOVERNANCE;
+  }
 
   return (
     <div>
-      <NavToMissionControl />
+      <NavToMissionControl href={href} text={text} />
       {id === undefined && (
         <BannerBlack
           text="Have you ever wondered where proposals come from? Join us in our open

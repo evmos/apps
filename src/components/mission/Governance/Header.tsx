6057925f699fbf01a722b815153281d9572d67eb
Mrsirdev <mr.sir.dev@gmail.com>
2023-03-29 13:09:06 -0500

    update buttons to be links

--- src/components/mission/Governance/Header.tsx
+++ src/components/mission/Governance/Header.tsx
@@ -1,41 +1,41 @@
-import dynamic from "next/dynamic";
+import Link from "next/link";
 import CommonWealthIcon from "../../footer/icons/CommonWealth";
-
-const Button = dynamic(() => import("../../common/Button"));
 
 const Header = () => {
   return (
     <div className="flex mb-6 w-full justify-between">
       <span className="text-lg text-pearl font-semibold">GOVERNANCE</span>
       <div className="flex gap-2">
-        <Button
-          onClick={() => {
-            console.log("click");
+        <Link
+          href={{
+            pathname: "/governance",
           }}
         >
-          <div className="flex items-center space-x-2 ">
+          <div className="flex justify-center text-pearl uppercase text-xs font-bold hover:bg-whiteOpacity border border-pearl p-2 rounded">
             <span>VOTE</span>
           </div>
-        </Button>
-        <Button
-          onClick={() => {
-            console.log("click");
-          }}
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href="https://evmos.community"
+          aria-label="docs"
         >
-          <div className="flex items-center space-x-2 ">
+          <div className="flex justify-center text-pearl uppercase text-xs font-bold hover:bg-whiteOpacity border border-pearl p-2 rounded">
             <span>DOCS</span>
           </div>
-        </Button>
-        <Button
-          onClick={() => {
-            console.log("click");
-          }}
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href="https://commonwealth.im/evmos"
+          aria-label="docs"
         >
-          <div className="flex items-center space-x-2 ">
+          <div className="flex justify-center text-pearl uppercase text-xs font-bold hover:bg-whiteOpacity border border-pearl p-2 rounded">
             <CommonWealthIcon width={16} height={16} />
             <span>DISCUSSION</span>
           </div>
-        </Button>
+        </Link>
       </div>
     </div>
   );

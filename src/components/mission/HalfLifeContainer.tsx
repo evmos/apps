bec04896153b82c654618c9947f9743076786077
Mrsirdev <111534516+Mrsirdev@users.noreply.github.com>
2023-04-03 15:11:41 -0500

    link half life to docs (#203)

--- src/components/mission/HalfLifeContainer.tsx
+++ src/components/mission/HalfLifeContainer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useEpochDay } from "../../internal/common/api/hooks/useEpochDay";
 import { useRemainingEpochs } from "../../internal/common/api/hooks/useRemainingEpochs";
 import { Countdown } from "../common/Countdown";
@@ -8,12 +9,17 @@ const HalfLifeContainer = () => {
   const miliSecondsPerDay = 86400000;
 
   return (
-    <>
+    <Link
+      target="_blank"
+      rel="noreferrer"
+      href="https://medium.com/evmos/the-evmos-token-model-edc07014978b#:~:text=Evmos%20is%20highly,deemed%20too%20low"
+      aria-label="half life"
+    >
       <div className=" bg-darkGray2 flex text-2xl flex-col p-5 border border-pearl font-bold rounded-2xl font-[GreyCliff] text-pearl ">
         <span className="text-sm font-normal font-[IBM]">The Half Life</span>
         <Countdown epochs={remainingEpochs * miliSecondsPerDay + epochs} />
       </div>
-    </>
+    </Link>
   );
 };
 

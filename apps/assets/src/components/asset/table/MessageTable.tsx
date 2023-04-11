852ec6aa42af9b4925d751bff478f302fa992687
Milagros Paoletti <milipaoletti@Milagross-MacBook-Air.local>
2023-04-11 12:39:36 +0200

    add class to MessageTable

--- apps/assets/src/components/asset/table/MessageTable.tsx
+++ apps/assets/src/components/asset/table/MessageTable.tsx
@@ -6,7 +6,7 @@ const MessageTable = ({
   amountCols: number;
 }) => {
   return (
-    <tr className="">
+    <tr className="assetOneItem">
       <td colSpan={amountCols} className="bg-darkGray2">
         <div className="my-4 flex items-center justify-center space-x-3 text-center font-[GreyCliff] font-semibold">
           {children}

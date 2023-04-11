5499bd97f292e4373fb7ed20be759a86cf68e1e9
Milagros Paoletti <milipaoletti@Milagross-MacBook-Air.local>
2023-04-11 13:11:39 +0200

    fix: bg stars

--- apps/governance/src/components/Container.tsx
+++ apps/governance/src/components/Container.tsx
@@ -1,7 +1,7 @@
 const Container = ({ children }: { children: JSX.Element }) => {
   return (
     <div className="grid min-h-screen bg-black">
-      <div className='flex h-full w-full flex-col overflow-auto bg-[url("/stars.svg")] bg-auto bg-center bg-repeat px-0 font-[GreyCliff] sm:px-5 xl:px-14'>
+      <div className='flex h-full w-full flex-col overflow-auto bg-[url("../public/stars.svg")] bg-auto bg-center bg-repeat px-0 font-[GreyCliff] sm:px-5 xl:px-14'>
         {children}
       </div>
     </div>

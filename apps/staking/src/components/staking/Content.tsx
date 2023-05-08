320c2a73e570ad17c384178f6ae2d23da8c28d77
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-05-08 11:14:50 +0200

    staking page: add track for click on back to mission control

--- apps/staking/src/components/staking/Content.tsx
+++ apps/staking/src/components/staking/Content.tsx
@@ -9,13 +9,19 @@ import { tabsContent } from "./Tabs/Content";
 import { NAV_TO_MISSION_CONTROL, EVMOS_PAGE_URL } from "constants-helper";
 const TopBarStaking = dynamic(() => import("./components/TopBarStaking"));
 const Tabs = dynamic(() => import("../common/tabComponent/Tabs"));
+import { CLICK_BACK_TO_MC, useTracker } from "tracker";
 
 const Content = () => {
+  const { handlePreClickAction } = useTracker(CLICK_BACK_TO_MC);
   return (
     <SearchWrapper>
       <ValidatorStateWrapper>
         <div className="">
-          <Navigation href={EVMOS_PAGE_URL} text={NAV_TO_MISSION_CONTROL} />
+          <Navigation
+            href={EVMOS_PAGE_URL}
+            text={NAV_TO_MISSION_CONTROL}
+            onClick={handlePreClickAction}
+          />
           <TopBarStaking />
           <div className=" xl:scrollbar-hide mt-5 max-h-[33vh] w-full overflow-y-auto px-2 font-[IBM] text-pearl sm:max-h-[45vh] lg:max-h-[53vh]">
             <Tabs

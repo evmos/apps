748eb91108d51f83e258f0c7a16c84143efa4b96
Sandoche <sandoche@protonmail.com>
2023-05-09 15:44:23 +0100

    workflow: adding new linting plugin for security

--- packages/eslint-config-custom/shared.js
+++ packages/eslint-config-custom/shared.js
@@ -10,6 +10,7 @@ module.exports = {
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:security/recommended"
   ],
   plugins: ["@typescript-eslint", "prettier"],
   parser: "@typescript-eslint/parser",

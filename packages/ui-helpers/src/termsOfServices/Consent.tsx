291d84ba185dac9fb148dc854c9a5b6e02eedb7c
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-05-24 10:03:23 +0200

    reuse createHtmlContent for Consent component

--- packages/ui-helpers/src/termsOfServices/Consent.tsx
+++ packages/ui-helpers/src/termsOfServices/Consent.tsx
@@ -1,9 +1,11 @@
 import Script from "next/script";
 import { useEffect } from "react";
+import { createHtmlContent } from "./helper";
 
 interface BrowserWindow {
   siteId: number;
   cookiePolicyId: number;
+  htmlContent: string;
 }
 
 export const Consent = () => {
@@ -20,15 +22,17 @@ export const Consent = () => {
         Number(process.env.NEXT_PUBLIC_SITE_ID_IUBENDA) ?? 0;
       browserWindow.cookiePolicyId =
         Number(process.env.NEXT_PUBLIC_COOKIE_POLICY_ID_IUBENDA) ?? 0;
+      browserWindow.htmlContent = createHtmlContent(
+        false,
+        "iubenda-cs-banner-consent"
+      );
       const script = document.createElement("script");
       script.id = "iubenda-script";
       script.type = "text/javascript";
       /* eslint-disable no-secrets/no-secrets */
       script.innerHTML = `var _iub = _iub || [];
-
         _iub.csConfiguration = {"callback":{"onReady":function(){document.getElementById('iubenda-cs-banner-consent').parentElement.remove();}},"askConsentAtCookiePolicyUpdate":true,"enableLgpd":true,"lang":"en","perPurposeConsent":true,"siteId":siteId,"whitelabel":false,"cookiePolicyId":cookiePolicyId,"banner":{"acceptButtonCaptionColor":"#000000","acceptButtonColor":"#E1DDD7","acceptButtonDisplay":true,"backgroundColor":"#FBF6EF","backgroundOverlay":true,"closeButtonDisplay":false,"explicitWithdrawal":true,"listPurposes":true,"logo":null,"position":"float-center","rejectButtonCaptionColor":"#000000","rejectButtonColor":"#E1DDD7","rejectButtonDisplay":true,"showPurposesToggles":true,"theme":"autumn-neutral","textColor":"#010436",
-        "html":"<div id='iubenda-cs-banner-consent' style='z-index:99999998 !important;' class='iubenda-cs-default-floating iubenda-cs-center iubenda-cs-overlay iubenda-cs-slidein iubenda-cs-visible' role='alertdialog' aria-labelledby='iubenda-cs-title' aria-describedby='iubenda-cs-paragraph'><div class='iubenda-cs-container iubenda-cs-themed'><div class='iubenda-cs-content' style='background-color: #FBF6EF !important;color: #010436 !important;font-size: 14px !important;'><div class='iubenda-cs-rationale'><button type='button' class='iubenda-cs-close-btn' tabindex='0' role='button' aria-pressed='false' style='display:none!important;'>×</button><div class='iubenda-banner-content iubenda-custom-content' role='document'><div id='iubenda-cs-title'>We value your privacy!</div><div id='iubenda-cs-paragraph'><p class='iub-p'>We use cookies to enhance your browsing experience and analyze our traffic. By clicking 'Accept All', you agree to our <a href=javascript:void(0) class='iubenda-privacy-policy-link'>privacy policy</a> and <a href=javascript:void(0) class='iubenda-cs-cookie-policy-lnk' target='_blank' rel='noopener'>cookie policy</a>.</p></div></div><div class='iubenda-cs-opt-group' style='color:#FBF6EF!important;'><div class='iubenda-cs-opt-group-consent'><button class='iubenda-cs-reject-btn iubenda-cs-btn-primary' tabindex='0' role='button' aria-pressed='false'>Reject</button><button class='iubenda-cs-accept-btn iubenda-cs-btn-primary' tabindex='0' role='button' aria-pressed='false'>Accept</button></div></div></div></div></div></div>" }}
-
+        "html": htmlContent}}
      `;
       /* eslint-enable no-secrets/no-secrets */
       script.async = true;
--- packages/ui-helpers/src/termsOfServices/helper.tsx
+++ packages/ui-helpers/src/termsOfServices/helper.tsx
@@ -1,4 +1,7 @@
-export const createHtmlContent = (withSetAction: boolean) => {
+export const createHtmlContent = (
+  withSetAction: boolean,
+  scriptId?: string
+) => {
   const acceptOnClick = withSetAction
     ? `(function(){setAction(true); var elem = document.querySelector('#iubenda-cs-banner'); elem.parentNode.removeChild(elem);})();`
     : `(function(){var elem = document.querySelector('#iubenda-cs-banner'); elem.parentNode.removeChild(elem); localStorage.setItem(disableTracker, 'false');})();`;
@@ -7,83 +10,40 @@ export const createHtmlContent = (withSetAction: boolean) => {
     ? `(function(){setAction(false); var elem = document.querySelector('#iubenda-cs-banner'); elem.parentNode.removeChild(elem);})();`
     : `(function(){var elem = document.querySelector('#iubenda-cs-banner'); elem.parentNode.removeChild(elem); localStorage.setItem(disableTracker, 'true');})();`;
 
-  return `
-      <div id="iubenda-cs-banner-tos" style="z-index:99999998 !important;" class="iubenda-cs-default-floating iubenda-cs-center 
-      iubenda-cs-overlay iubenda-cs-slidein iubenda-cs-visible" role="alertdialog" aria-labelledby="iubenda-cs-title" aria-describedby="iubenda-cs-paragraph">
-        <div id="iubenda-cs-banner" style="z-index:99999998 !important" class="iubenda-cs-default-floating iubenda-cs-center iubenda-cs-overlay iubenda-cs-slidein 
-        iubenda-cs-visible" role="alertdialog" aria-labelledby="iubenda-cs-title" aria-describedby="iubenda-cs-paragraph">
-          <div class="iubenda-cs-container iubenda-cs-themed">
-            <div class="iubenda-cs-content" style="background-color: #FBF6EF !important;color: #010436 !important;font-size: 14px !important;">
-              <div class="iubenda-cs-rationale">
-                <button type="button" class="iubenda-cs-close-btn" tabindex="0" role="button" aria-pressed="false" style="display:none!important;">×</button>
-                <div class="iubenda-banner-content iubenda-custom-content" role="document">
-                  <div id="iubenda-cs-title">We value your privacy!</div>
-                  <div id="iubenda-cs-paragraph">
-                    <p class="iub-p">We use cookies to enhance your browsing experience and analyze our traffic. By clicking 'Accept All', you agree to our 
-                    <a href="https://evmos.org/privacy-policy" class="iubenda-privacy-policy-link" target="_blank">privacy policy</a> and 
-                    <a href="https://evmos.org/cookie-policy" class="iubenda-cs-cookie-policy-lnk" target="_blank" rel="noopener">cookie policy</a>.</p>
-                  </div>
-                </div>
-                <div class="iubenda-cs-opt-group" style="color:#FBF6EF!important;">
-                  <div class="iubenda-cs-opt-group-consent">
-                    <button onclick="${acceptOnClick}" class="iubenda-cs-accept-btn iubenda-cs-btn-primary" tabindex="0" role="button" aria-pressed="false">Accept</button>
-                    <button onclick="${rejectOnClick}" class="iubenda-cs-reject-btn iub-prevent-consent iubenda-cs-btn-primary" tabindex="0" role="button" aria-pressed="false" >Reject</button>
-                  </div>
-                </div>
-              </div>
+  const id = scriptId ? scriptId : "iubenda-cs-banner";
+
+  const content = ` <div id=${id} style="z-index:99999998 !important" class="iubenda-cs-default-floating iubenda-cs-center iubenda-cs-overlay iubenda-cs-slidein 
+  iubenda-cs-visible" role="alertdialog" aria-labelledby="iubenda-cs-title" aria-describedby="iubenda-cs-paragraph">
+    <div class="iubenda-cs-container iubenda-cs-themed">
+      <div class="iubenda-cs-content" style="background-color: #FBF6EF !important;color: #010436 !important;font-size: 14px !important;">
+        <div class="iubenda-cs-rationale">
+          <button type="button" class="iubenda-cs-close-btn" tabindex="0" role="button" aria-pressed="false" style="display:none!important;">×</button>
+          <div class="iubenda-banner-content iubenda-custom-content" role="document">
+            <div id="iubenda-cs-title">We value your privacy!</div>
+            <div id="iubenda-cs-paragraph">
+              <p class="iub-p">We use cookies to enhance your browsing experience and analyze our traffic. By clicking 'Accept All', you agree to our 
+              <a href="https://evmos.org/privacy-policy" class="iubenda-privacy-policy-link" target="_blank">privacy policy</a> and 
+              <a href="https://evmos.org/cookie-policy" class="iubenda-cs-cookie-policy-lnk" target="_blank" rel="noopener">cookie policy</a>.</p>
+            </div>
+          </div>
+          <div class="iubenda-cs-opt-group" style="color:#FBF6EF!important;">
+            <div class="iubenda-cs-opt-group-consent">
+              <button onclick="${acceptOnClick}" class="iubenda-cs-accept-btn iubenda-cs-btn-primary" tabindex="0" role="button" aria-pressed="false">Accept</button>
+              <button onclick="${rejectOnClick}" class="iubenda-cs-reject-btn iub-prevent-consent iubenda-cs-btn-primary" tabindex="0" role="button" aria-pressed="false" >Reject</button>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  </div>`;
+  if (scriptId) {
+    return content;
+  } else {
+    return `
+      <div id="iubenda-cs-banner-tos" style="z-index:99999998 !important;" class="iubenda-cs-default-floating iubenda-cs-center 
+      iubenda-cs-overlay iubenda-cs-slidein iubenda-cs-visible" role="alertdialog" aria-labelledby="iubenda-cs-title" aria-describedby="iubenda-cs-paragraph">
+       ${content}
+      </div>
     `;
+  }
 };
-
-// export const createHtmlContent = (withSetAction: boolean) => {
-//   if (withSetAction) {
-//     // this code handles the consent modal inside the TOS modal. We have to update the checkbox state when the user clicks on accept or reject
-//     return `<div id="iubenda-cs-banner-tos" style="z-index:99999998 !important;" class="iubenda-cs-default-floating iubenda-cs-center
-//     iubenda-cs-overlay iubenda-cs-slidein iubenda-cs-visible" role="alertdialog" aria-labelledby="iubenda-cs-title" aria-describedby="iubenda-cs-paragraph">
-//     <div id="iubenda-cs-banner" style="z-index:99999998 !important" class="iubenda-cs-default-floating iubenda-cs-center iubenda-cs-overlay iubenda-cs-slidein
-//     iubenda-cs-visible" role="alertdialog" aria-labelledby="iubenda-cs-title" aria-describedby="iubenda-cs-paragraph">
-//     <div class="iubenda-cs-container iubenda-cs-themed"><div class="iubenda-cs-content" style="background-color: #FBF6EF !important;color: #010436 !important;
-//     font-size: 14px !important;"><div class="iubenda-cs-rationale"><button type="button" class="iubenda-cs-close-btn" tabindex="0" role="button" aria-pressed="false"
-//     style="display:none!important;">×</button>
-//     <div class="iubenda-banner-content iubenda-custom-content" role="document"><div id="iubenda-cs-title">We value your privacy!</div><div id="iubenda-cs-paragraph">
-//     <p class="iub-p">We use cookies to enhance your browsing experience and analyze our traffic. By clicking 'Accept All', you agree to our
-//     <a href="https://evmos.org/privacy-policy" class="iubenda-privacy-policy-link" target="_blank">privacy policy</a> and
-//     <a href="https://evmos.org/cookie-policy" class="iubenda-cs-cookie-policy-lnk" target="_blank"
-//     rel="noopener">cookie policy</a>.</p></div></div><div class="iubenda-cs-opt-group" style="color:#FBF6EF!important;">
-//     <div class="iubenda-cs-opt-group-consent">
-//     <button onclick="(function(){setAction(true); var elem = document.querySelector('#iubenda-cs-banner');
-//     elem.parentNode.removeChild(elem);})();" class="iubenda-cs-accept-btn iubenda-cs-btn-primary" tabindex="0"
-//   role="button" aria-pressed="false">Accept</button>
-//     <button class="iubenda-cs-reject-btn iub-prevent-consent iubenda-cs-btn-primary" tabindex="0" role="button" aria-pressed="false"
-//     onclick="(function(){setAction(false);
-//     var elem = document.querySelector('#iubenda-cs-banner'); elem.parentNode.removeChild(elem);})();">Reject</button>
-//   </div></div></div></div></div></div></div>`;
-//   }
-//   // this code handles the modal that appears when the user clicks on cookies settings (footer). We don't have to update the checkbox state.
-//   // we only update the disableTracker value that is stored in localStorage
-//   /* eslint-disable no-secrets/no-secrets */
-//   return `<div id="iubenda-cs-banner-evmos" style="z-index:99999998 !important;" class="iubenda-cs-default-floating iubenda-cs-center
-//     iubenda-cs-overlay iubenda-cs-slidein iubenda-cs-visible" role="alertdialog" aria-labelledby="iubenda-cs-title" aria-describedby="iubenda-cs-paragraph">
-//     <div id="iubenda-cs-banner" style="z-index:99999998 !important" class="iubenda-cs-default-floating iubenda-cs-center iubenda-cs-overlay iubenda-cs-slidein
-//     iubenda-cs-visible" role="alertdialog" aria-labelledby="iubenda-cs-title" aria-describedby="iubenda-cs-paragraph">
-//     <div class="iubenda-cs-container iubenda-cs-themed"><div class="iubenda-cs-content" style="background-color: #FBF6EF !important;color: #010436 !important;
-//     font-size: 14px !important;"><div class="iubenda-cs-rationale"><button type="button" class="iubenda-cs-close-btn" tabindex="0" role="button" aria-pressed="false"
-//     style="display:none!important;">×</button>
-//     <div class="iubenda-banner-content iubenda-custom-content" role="document"><div id="iubenda-cs-title">We value your privacy!</div><div id="iubenda-cs-paragraph">
-//     <p class="iub-p">We use cookies to enhance your browsing experience and analyze our traffic. By clicking 'Accept All', you agree to our
-//     <a href="https://evmos.org/privacy-policy" class="iubenda-privacy-policy-link" target="_blank">privacy policy</a> and
-//     <a href="https://evmos.org/cookie-policy" class="iubenda-cs-cookie-policy-lnk" target="_blank"
-//     rel="noopener">cookie policy</a>.</p></div></div><div class="iubenda-cs-opt-group" style="color:#FBF6EF!important;">
-//     <div class="iubenda-cs-opt-group-consent">
-//     <button onclick="(function(){var elem = document.querySelector('#iubenda-cs-banner');
-//     elem.parentNode.removeChild(elem); localStorage.setItem(disableTracker, 'false')})();" class="iubenda-cs-accept-btn iubenda-cs-btn-primary" tabindex="0"
-//     role="button" aria-pressed="false">Accept</button>
-//     <button class="iubenda-cs-reject-btn iub-prevent-consent iubenda-cs-btn-primary" tabindex="0" role="button" aria-pressed="false"
-//     onclick="(function(){
-//     var elem = document.querySelector('#iubenda-cs-banner'); elem.parentNode.removeChild(elem);  localStorage.setItem(disableTracker, 'true')})();">Reject</button>
-//     </div></div></div></div></div></div></div>`;
-//   /* eslint-enable no-secrets/no-secrets */
-// };

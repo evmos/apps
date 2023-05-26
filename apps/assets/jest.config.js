ebd8b1225cc19c2303fddec34b75955205659f52
Sandoche <sandoche@protonmail.com>
2023-05-26 16:17:26 +0100

    fix: fixing jest so that the tests can pass

--- apps/assets/jest.config.js
+++ apps/assets/jest.config.js
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 const nextJest = require("next/jest");
-const sharedConfig = require("jest-config-custom/jest.apps.config");
+const appsConfig = require("jest-config-custom/jest.apps.config");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -12,7 +12,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  ...sharedConfig,
+  ...appsConfig,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
--- packages/jest-config-custom/jest.apps.config.js
+++ packages/jest-config-custom/jest.apps.config.js
@@ -3,7 +3,7 @@ const sharedConfig = require("./jest.shared.config.js");
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     ...sharedConfig,
 }

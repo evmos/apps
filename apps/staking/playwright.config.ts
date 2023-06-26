d85dc844c291a368551a80c6e2e622d947cf2c81
Sandoche <sandoche@protonmail.com>
2023-06-26 16:38:06 +0200

    tests: fixing e2e tests and adding gh action

---
+++ apps/staking/playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "@playwright/test";
+
+// @ts-ignore
+import { sharedConfig } from "playwright-config-custom";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  ...sharedConfig,
+});

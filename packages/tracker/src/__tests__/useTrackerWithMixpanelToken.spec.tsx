c1430453c95406a8a85ccb7e438954b241dce96f
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-06-30 16:42:03 +0200

    add use Tracker testing and check that track is called correctly after changing consent modal options

---
+++ packages/tracker/src/__tests__/useTrackerWithMixpanelToken.spec.tsx
@@ -0,0 +1,40 @@
+import { test, describe, expect, vi } from "vitest";
+import { renderHook, act } from "@testing-library/react";
+import { useTracker } from "../useTracker";
+import { MixpanelProvider } from "../MixPanelProvider";
+import mixpanel from "mixpanel-browser";
+
+vi.mock("mixpanel-browser", () => {
+  return {
+    default: {
+      init: vi.fn(),
+      config: {},
+      track: vi.fn(),
+    },
+  };
+});
+
+describe("useTracker", () => {
+  const wrapper = ({ children }: { children: JSX.Element }) => {
+    return (
+      <MixpanelProvider token="testToken" config={{ ip: false }}>
+        {children}
+      </MixpanelProvider>
+    );
+  };
+  test("should call mixpanel.track", () => {
+    const { result } = renderHook(
+      () => useTracker("event", { prop: "value" }),
+      { wrapper }
+    );
+    /* eslint-disable */
+    expect(mixpanel.init).toHaveBeenCalledTimes(1);
+    act(() => {
+      result.current.handlePreClickAction({ extraProp: "extraValue" });
+    });
+    expect(mixpanel.track).toHaveBeenCalledWith("event", {
+      prop: "value",
+      extraProp: "extraValue",
+    });
+  });
+});
--- packages/ui-helpers/src/__tests__/ConsentModal.spec.tsx
+++ packages/ui-helpers/src/__tests__/ConsentModal.spec.tsx
@@ -4,7 +4,19 @@ import userEvent from "@testing-library/user-event";
 import { vi } from "vitest";
 import { ConsentModal } from "../termsOfServices/ConsentModal";
 import { DISABLE_TRACKER_LOCALSTORAGE } from "tracker";
+import { MixpanelProvider, useTracker } from "tracker";
+import { renderHook, act } from "@testing-library/react";
+import mixpanel from "mixpanel-browser";
 
+vi.mock("mixpanel-browser", () => {
+  return {
+    default: {
+      init: vi.fn(),
+      config: {},
+      track: vi.fn(),
+    },
+  };
+});
 describe("Consent Modal", () => {
   beforeEach(() => {
     vi.clearAllMocks();
@@ -16,17 +28,51 @@ describe("Consent Modal", () => {
     vi.restoreAllMocks();
   });
 
+  const wrapper = ({ children }: { children: JSX.Element }) => {
+    return (
+      <MixpanelProvider token="testToken" config={{ ip: false }}>
+        {children}
+      </MixpanelProvider>
+    );
+  };
   const setShow = vi.fn();
-  const { getByRole } = render(<ConsentModal setShow={setShow} />);
+  const { getByRole } = render(<ConsentModal setShow={setShow} />, { wrapper });
+
   test("should set localstorage to false when clicks on accept", async () => {
     const button = getByRole("button", { name: /accept/i });
     await userEvent.click(button);
     expect(localStorage.getItem(DISABLE_TRACKER_LOCALSTORAGE)).toBe("false");
+
+    const { result } = renderHook(
+      () => useTracker("event", { prop: "value" }),
+      { wrapper }
+    );
+    /* eslint-disable */
+    expect(mixpanel.init).toHaveBeenCalledTimes(1);
+
+    act(() => {
+      result.current.handlePreClickAction({ extraProp: "extraValue" });
+    });
+    expect(mixpanel.track).toHaveBeenCalledWith("event", {
+      prop: "value",
+      extraProp: "extraValue",
+    });
   });
 
   test("should set localstorage to true when clicks on reject", async () => {
     const button = getByRole("button", { name: /reject/i });
     await userEvent.click(button);
     expect(localStorage.getItem(DISABLE_TRACKER_LOCALSTORAGE)).toBe("true");
+
+    const { result } = renderHook(
+      () => useTracker("event", { prop: "value" }),
+      { wrapper }
+    );
+
+    expect(mixpanel.init).toHaveBeenCalledTimes(1);
+    act(() => {
+      result.current.handlePreClickAction({ extraProp: "extraValue" });
+    });
+    expect(mixpanel.track).not.toHaveBeenCalled();
   });
 });

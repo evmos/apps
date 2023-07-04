f6dde60b0b93e1489b8f458a6a8e5bddedd36dd4
Sandoche <sandoche@protonmail.com>
2023-07-04 11:06:39 +0200

    ci: fixing tests github actions

--- apps/vesting/playwright.config.ts
+++ apps/vesting/playwright.config.ts
@@ -7,7 +7,7 @@ import { sharedConfig } from "playwright-config-custom";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require("dotenv").config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -16,7 +16,7 @@ export default defineConfig({
   ...sharedConfig,
   webServer: {
     command: "yarn start",
-    url: "http://127.0.0.1:3005",
+    port: 3005,
     reuseExistingServer: !process.env.CI,
     stdout: "pipe",
     stderr: "pipe",

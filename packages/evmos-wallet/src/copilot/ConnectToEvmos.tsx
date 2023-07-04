0af2df13964fb1b8f00c3d085d91f5fa50b5c218
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-07-04 16:41:32 +0200

    remove pr-2 in connectToEvmos

--- packages/evmos-wallet/src/copilot/ConnectToEvmos.tsx
+++ packages/evmos-wallet/src/copilot/ConnectToEvmos.tsx
@@ -2,7 +2,7 @@ import { EvmosRedIcon } from "icons";
 
 export const ConnectToEvmos = () => {
   return (
-    <div className="space-y-3 px-4 pr-2 pb-4 pt-5 sm:p-6">
+    <div className="space-y-3 px-4 pb-4 pt-5 sm:p-6">
       <EvmosRedIcon />
       <h3 className="text-gray-900 text-base font-semibold leading-6">
         Connect your account

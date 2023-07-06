58fda4770659c4e6a1e7d64b975d9ad224159785
Sandoche <sandoche@protonmail.com>
2023-07-06 13:51:05 +0200

    ci: adding script to upload the results of the e2e and unit tests

--- packages/vitest-config-custom/vitest.shared.config.ts
+++ packages/vitest-config-custom/vitest.shared.config.ts
@@ -15,5 +15,8 @@ module.exports = {
       "**/.{idea,git,cache,output,temp}/**",
       "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
     ],
+    coverage: {
+      reporter: ["text", "json-summary", "json"],
+    },
   },
 };

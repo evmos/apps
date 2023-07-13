c03b42df10ea12f5d1f583f1a2c650faad177eb6
Sandoche <sandoche@protonmail.com>
2023-07-13 09:03:46 +0200

    feat: refactoring the component to make it cleaner

--- packages/evmos-wallet/src/index.tsx
+++ packages/evmos-wallet/src/index.tsx
@@ -1,26 +1,6 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import enTranslation from "../locales/en/common.json";
-
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources: {
-      en: {
-        translation: enTranslation,
-      },
-    },
-    lng: "en", // if you're using a language detector, do not define the lng option
-    fallbackLng: "en",
-
-    interpolation: {
-      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
-    },
-  });
-
 // styles
 import "./styles.css";
 
@@ -100,3 +80,7 @@ export type { executedTx } from "./notification/transactionsTypes";
 export { TransactionStatus } from "./notification/transactionsTypes";
 export type { txStatusErrorResponse } from "./notification/transactionsTypes";
 export type { txStatusResponse } from "./notification/transactionsTypes";
+
+import { setup } from "./setup";
+
+setup();
---
+++ packages/evmos-wallet/src/setup.ts
@@ -0,0 +1,24 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
+
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslation from "../locales/en/common.json";
+
+export const setup = async () => {
+  i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+      resources: {
+        en: {
+          translation: enTranslation,
+        },
+      },
+      lng: "en", // if you're using a language detector, do not define the lng option
+      fallbackLng: "en",
+
+      interpolation: {
+        escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+      },
+    });
+};

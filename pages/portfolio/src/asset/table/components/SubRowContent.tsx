528a3c7ef0cb883bde56cc22870328b7affb1eb2
Renzo <renzobarrionuevo@gmail.com>
2024-02-08 11:48:56 -0300

    remove IBC sub-balance

--- pages/portfolio/src/asset/table/components/SubRowContent.tsx
+++ pages/portfolio/src/asset/table/components/SubRowContent.tsx
@@ -1,10 +1,7 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
-import { BigNumber } from "@ethersproject/bignumber";
-import Link from "next/link";
 import { amountToDollars, convertAndFormat } from "helpers";
-import { EVMOS_SYMBOL } from "@evmosapps/evmos-wallet";
 import { Tooltip } from "@evmosapps/ui-helpers";
 import { QuestionMarkIcon } from "@evmosapps/icons/QuestionMarkIcon";
 import { Description } from "./Description";
@@ -16,41 +13,6 @@ export const SubRowContent = ({ item }: SubRowProps) => {
   const symbol = item.symbol;
   const description = item.description;
   const img = item.pngSrc;
-
-  const createV10Tooltip = () => {
-    return (
-      <div
-        className={`text-xs capitalize ${
-          item.cosmosBalance.eq(BigNumber.from("0")) ||
-          item.symbol === EVMOS_SYMBOL
-            ? "hidden"
-            : ""
-        }`}
-      >
-        <Tooltip
-          className="w-24"
-          element={<QuestionMarkIcon width={16} height={16} />}
-          text={
-            <>
-              Since{" "}
-              <Link
-                className="text-red-300"
-                href={v10Link}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                v10
-              </Link>{" "}
-              upgrade, all withdraws will pull first from IBC token balance
-              before ERC-20. Deposits are autoconverted to ERC-20
-            </>
-          }
-        />
-      </div>
-    );
-  };
-  const v10Link =
-    "https://commonwealth.im/evmos/discussion/8501-evmos-software-upgrade-v10";
 
   const createCoingeckoAlert = useCallback(() => {
     return (
@@ -95,27 +57,6 @@ export const SubRowContent = ({ item }: SubRowProps) => {
           <span className="break-all text-base font-bold">
             {convertAndFormat(balance, item.decimals, 6)}
           </span>
-          {/* displays ibc balance */}
-          <div
-            className={` ${
-              item.cosmosBalance.eq(BigNumber.from("0")) ||
-              item.symbol === EVMOS_SYMBOL
-                ? "hidden"
-                : ""
-            } flex items-center justify-end font-bold lg:justify-start`}
-          >
-            <div className="block pr-1 lg:hidden">{createV10Tooltip()}</div>
-            <Tooltip
-              className="capitalize"
-              element={
-                <p className="break-all text-sm opacity-80">
-                  {convertAndFormat(item.cosmosBalance, item.decimals)}{" "}
-                </p>
-              }
-              text="IBC Balance"
-            />
-            <div className="hidden pl-1 lg:block">{createV10Tooltip()}</div>
-          </div>
 
           {/* displays erc20 tokens in dollars */}
           {/* stevmos uses evmos price until they add it on coingecko. 

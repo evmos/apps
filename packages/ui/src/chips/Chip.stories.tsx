41433b717e76652338d7af42f71261e893e0aeae
Renzo <renzobarrionuevo@gmail.com>
2024-03-27 19:14:16 -0300

    trailing chips

--- packages/ui/src/chips/Chip.stories.tsx
+++ packages/ui/src/chips/Chip.stories.tsx
@@ -1,23 +1,128 @@
-// Copyright Tharsis Labs Ltd.(Evmos)
-// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
-
 import type { Meta, StoryObj } from "@storybook/react";
+import * as icons from "../icons/line";
+import { Fragment } from "react";
 import { Chip } from "./Chip";
-import { IconCheckboxCircle } from "../icons/line";
-
+const variants = ["trailing"] as const;
 const meta = {
-  title: "Chip",
+  title: "Chips",
   component: Chip,
-  tags: ["autodocs"],
-  argTypes: { onClick: { action: "clicked" } },
+  parameters: {
+    layout: "centered",
+  },
 } satisfies Meta<typeof Chip>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+const labelText = "Label";
+
+export const Chips: Story = {
   args: {
-    icon: <IconCheckboxCircle className="w-5 h-5" />,
+    //change icon
+    icon: "IconCheckboxCircle",
     children: "Label",
+  },
+  argTypes: {
+    icon: {
+      options: Object.keys(icons),
+      control: { type: "select" },
+    },
+  },
+  render: (props) => {
+    const Icon = icons[props.icon as keyof typeof icons];
+
+    return (
+      <div className="grid text-center grid-cols-5 gap-4 [&>*]:flex [&>*]:items-center [&>*]:justify-center">
+        {variants.map((variant) => (
+          <Fragment key={variant}>
+            <div className="col-span-5 h-14">{variant}</div>
+            {/* colums */}
+            <div></div>
+            <div>Neutral</div>
+            <div>Success</div>
+            <div>Warning</div>
+            <div>Error</div>
+            {/* rows */}
+            <div>{"Default"}</div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} default>
+                <Icon />
+              </Chip>
+            </div>
+            <div>
+              <Chip
+                {...props}
+                variant={variant}
+                text={labelText}
+                Success={true}
+                default
+              >
+                <Icon />
+              </Chip>
+            </div>
+            <div>
+              <Chip
+                {...props}
+                variant={variant}
+                text={labelText}
+                Warning
+                default
+              >
+                <Icon />
+              </Chip>
+            </div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} Error default>
+                <Icon />
+              </Chip>
+            </div>
+
+            <div>{"Hover"}</div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} hover default>
+                <Icon />
+              </Chip>
+            </div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} hover Success>
+                <Icon />
+              </Chip>
+            </div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} hover Warning>
+                <Icon />
+              </Chip>
+            </div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} hover Error>
+                <Icon />
+              </Chip>
+            </div>
+            <div>{"Focus"}</div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} focus default>
+                <Icon />
+              </Chip>
+            </div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} focus Success>
+                <Icon />
+              </Chip>
+            </div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} focus Warning>
+                <Icon />
+              </Chip>
+            </div>
+            <div>
+              <Chip {...props} variant={variant} text={labelText} focus Error>
+                <Icon />
+              </Chip>
+            </div>
+          </Fragment>
+        ))}
+      </div>
+    );
   },
 };
--- packages/ui/src/chips/Chip.tsx
+++ packages/ui/src/chips/Chip.tsx
@@ -1,20 +1,75 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
-import { ComponentProps } from "react";
+import { VariantProps, cva } from "cva";
+import { cn } from "helpers/src/classnames";
+import { ElementType, createElement, forwardRef } from "react";
 
-export const Chip = ({
-  icon,
+const chip = cva({
+  base: [
+    "flex items-center w-fit h-[28px] pl-[10px] pr-2 py-2 rounded-2xl text-sm gap-x-2",
+  ],
+  variants: {
+    // size: {
+    //   sm: "",
+    //   md: "w-11 [&_.icon]:h-5 [&_.icon]:w-5",
+    //   lg: "w-14 [&_.icon]:h-6 [&_.icon]:w-6",
+    // },
+    variant: {
+      trailing: [
+        "bg-on-surface dark:bg-on-surface-dark [&_.icon]:h-3 [&_.icon]:w-3",
+      ],
+    },
+    default: {
+      true: "bg-opacity-[0.08] dark:bg-opacity-[0.08] text-on-surface-variant dark:text-on-surface-variant-dark",
+    },
+    hover: {
+      true: "bg-opacity-[0.2] dark:bg-opacity-[0.2]",
+    },
+    focus: {
+      true: "bg-opacity-[0.2] dark:bg-opacity-[0.2]",
+    },
+    Success: {
+      true: "bg-success-light dark:bg-success-dark text-success dark:text-success-dark",
+    },
+    Warning: {
+      true: "bg-warning-light dark:bg-warning-dark text-warning dark:text-warning-dark",
+    },
+    Error: {
+      true: "bg-error-light dark:bg-error-dark text-error dark:text-error-dark",
+    },
+  },
+});
+
+export interface ChipStyleProps extends VariantProps<typeof chip> {}
+
+export type ChipProps<T extends ElementType> = {
+  as?: T;
+  className?: string;
+  text: string;
+} & React.ComponentPropsWithRef<T> &
+  ChipStyleProps;
+
+function _Chips<T extends ElementType = "button">({
+  as,
+  className,
+  text,
   children,
-  ...rest
-}: ComponentProps<"span"> & { icon?: JSX.Element }) => {
-  return (
-    <span
-      className="flex items-center w-fit px-3 py-1 bg-surface-container-high dark:bg-surface-container-high-dark rounded-2xl text-on-surface-variant dark:text-paragraph-dark text-sm leading-none"
-      {...rest}
-    >
-      {icon && <span className="mr-2">{icon}</span>}
+  ...props
+}: ChipProps<T>) {
+  return createElement(
+    (as as string) ?? "button",
+    {
+      ...props,
+      className: cn(chip(props), className as string),
+    },
+    <>
+      {text}
       {children}
-    </span>
+    </>,
   );
-};
+}
+
+export const Chip = forwardRef(_Chips) as <T extends ElementType = "button">(
+  props: ChipProps<T>,
+) => JSX.Element;

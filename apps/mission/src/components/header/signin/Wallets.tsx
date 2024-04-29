ca6678d7e476bbb94c2e6971f29fd54741e56b97
Milagros Paoletti <milagros.paoletti@gmail.com>
2024-04-29 16:53:47 +0200

    clean up wallets

--- apps/mission/src/components/header/signin/Wallets.tsx
+++ apps/mission/src/components/header/signin/Wallets.tsx
@@ -5,37 +5,32 @@
 
 import { WALLETS_TYPE, useSignIn } from "./useSignIn";
 import { useMemo, useState } from "react";
-import {
-  getActiveProviderKey,
-  getGlobalKeplrProvider,
-  useWallet,
-} from "@evmosapps/evmos-wallet";
+import { getActiveProviderKey, useWallet } from "@evmosapps/evmos-wallet";
 import { Spinner } from "@evmosapps/ui/components/spinners/Spinner.tsx";
 import { IconCross } from "@evmosapps/ui/icons/line/basic/cross.tsx";
 import { IconCheck } from "@evmosapps/ui/icons/line/basic/check.tsx";
 import { useInstallProvider } from "./useWalletInstalled";
 import { Dropdown } from "@evmosapps/ui/components/dropdown/Dropdown.tsx";
 import { IconButton } from "@evmosapps/ui/button/icon-button.tsx";
-import { getGlobalLeapProvider } from "@evmosapps/evmos-wallet/src/wallet/utils/leap/getLeapProvider";
 import { Connector } from "wagmi";
 import { useTranslation } from "@evmosapps/i18n/client";
+import { isKeplrInstalled, isLeapInstalled } from "./helpers";
 
 export const Wallets = ({ wallets }: { wallets: WALLETS_TYPE[] }) => {
   const { connectors, connect, error } = useSignIn();
   const { isConnecting, isConnected } = useWallet();
   const [walletSelectedToConnect, setWalletSelectedToConnect] = useState("");
-  const { t } = useTranslation("dappStore");
   const [setProviderStatus] = useInstallProvider(walletSelectedToConnect);
+  const { t } = useTranslation("dappStore");
 
   const isWalletDetected = (
     wallet: string,
     connector: Connector | undefined,
   ) => {
     if (
-      (wallet === "Keplr" && getGlobalKeplrProvider() === null) ||
-      (wallet === "Leap" && getGlobalLeapProvider() === null) ||
-      (isConnected &&
-        getActiveProviderKey()?.toLowerCase() === wallet.toLowerCase()) ||
+      (wallet === "Keplr" && !isKeplrInstalled()) ||
+      (wallet === "Leap" && !isLeapInstalled()) ||
+      (isConnected && isWalletconnected(wallet)) ||
       isConnected ||
       error ||
       isConnecting ||
@@ -46,20 +41,62 @@ export const Wallets = ({ wallets }: { wallets: WALLETS_TYPE[] }) => {
     return true;
   };
 
-  const tempWallet = useMemo(() => {
+  const isWalletRecommended = (wallet: string) => {
+    return wallet === "MetaMask" && !error && !isConnected && !isConnecting;
+  };
+
+  const hasError = (wallet: string) => {
+    return error && error.name === wallet;
+  };
+
+  const isWalletLoading = (wallet: string) => {
+    return isConnecting && walletSelectedToConnect === wallet && !error;
+  };
+
+  const isWalletconnected = (wallet: string) => {
+    return getActiveProviderKey()?.toLowerCase() === wallet.toLowerCase();
+  };
+
+  const isWalletDisabled = (wallet: string) => {
+    return (isConnected || isConnecting) && getActiveProviderKey() === wallet;
+  };
+
+  const handleConnect = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    connector: Connector | undefined,
+    wallet: WALLETS_TYPE,
+  ) => {
+    if (wallet === undefined) return;
+    e.preventDefault();
+
+    if (
+      !connector ||
+      (connector && wallet.name === "Keplr" && !isKeplrInstalled()) ||
+      (connector && wallet.name === "Leap" && !isLeapInstalled())
+    ) {
+      window.open(wallet.url, "_blank");
+      setProviderStatus("started-install");
+      localStorage.setItem("redirect-wallet", "true");
+    } else {
+      connect({ connector });
+      setWalletSelectedToConnect(connector.name);
+    }
+  };
+
+  const listWallets = useMemo(() => {
     if (getActiveProviderKey() !== null) {
       const indexForWallet = wallets.findIndex(
         (wallet) => wallet?.name === getActiveProviderKey(),
       );
-      const tempWallet = wallets.slice();
-      const selectedWallet = tempWallet.splice(indexForWallet, 1)[0];
-      tempWallet.unshift(selectedWallet);
-      return tempWallet;
+      const listWallets = wallets.slice();
+      const selectedWallet = listWallets.splice(indexForWallet, 1)[0];
+      listWallets.unshift(selectedWallet);
+      return listWallets;
     }
     return wallets;
   }, [wallets]);
 
-  return tempWallet?.map((wallet) => {
+  return listWallets?.map((wallet) => {
     if (!wallet) return;
     const Icon = wallet.icon as React.FC<React.SVGAttributes<SVGElement>>;
     const connector = connectors.find((c) => c.name === wallet.name);
@@ -67,48 +104,24 @@ export const Wallets = ({ wallets }: { wallets: WALLETS_TYPE[] }) => {
     return (
       <Dropdown.Item
         as="button"
-        disabled={
-          (isConnected || isConnecting) &&
-          getActiveProviderKey() === wallet.name
-        }
+        disabled={isWalletDisabled(wallet.name)}
         key={wallet.name}
-        onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-          e.preventDefault();
-
-          if (connector) {
-            if (
-              (wallet.name === "Keplr" && getGlobalKeplrProvider() === null) ||
-              (wallet.name === "Leap" && getGlobalLeapProvider() === null)
-            ) {
-              window.open(wallet.url, "_blank");
-              setProviderStatus("started-install");
-              localStorage.setItem("redirect-wallet", "true");
-            } else {
-              connect({ connector });
-              setWalletSelectedToConnect(connector.name);
-            }
-          } else {
-            window.open(wallet.url, "_blank");
-            setProviderStatus("started-install");
-            localStorage.setItem("redirect-wallet", "true");
-          }
-        }}
+        onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+          handleConnect(e, connector, wallet)
+        }
       >
         {Icon && <Icon className="w-7 h-7 shrink-0" />}
         <div className="text-left flex justify-between w-full items-center ">
           <div className="text-sm">
             {wallet.displayName}
-            {wallet.name === "MetaMask" &&
-              !error &&
-              !isConnected &&
-              !isConnecting && (
-                <div className="text-paragraph dark:text-paragraph-dark text-xs">
-                  {t("signIn.wallet.recommended")}
-                </div>
-              )}
-            {error && error.name === wallet.name && (
+            {isWalletRecommended(wallet.name) && (
+              <div className="text-paragraph dark:text-paragraph-dark text-xs">
+                {t("signIn.wallet.recommended")}
+              </div>
+            )}
+            {hasError(wallet.name) && (
               <div className="text-error-container dark:text-error-container-dark  text-xs font-medium leading-4">
-                {error.error}
+                {error?.error}
               </div>
             )}
           </div>
@@ -117,17 +130,14 @@ export const Wallets = ({ wallets }: { wallets: WALLETS_TYPE[] }) => {
               {t("signIn.wallet.detected")}
             </p>
           )}
-          {isConnecting &&
-            walletSelectedToConnect === wallet.name &&
-            !error && <Spinner />}
-          {getActiveProviderKey()?.toLowerCase() ===
-            wallet.name.toLowerCase() && (
+          {isWalletLoading(wallet.name) && <Spinner />}
+          {isWalletconnected(wallet.name) && (
             <IconButton as="div" variant="success" size="sm">
               <IconCheck />
             </IconButton>
           )}
 
-          {error && error.name === wallet.name && (
+          {hasError(wallet.name) && (
             <IconButton as="div" variant="danger" size="sm">
               <IconCross />
             </IconButton>

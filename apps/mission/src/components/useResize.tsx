c625b9252db0086099c808ce39ebfe3f571d8bf0
Milagros Paoletti <40247040+miliPaoletti@users.noreply.github.com>
2024-05-03 10:25:14 +0200

    Update apps/mission/src/components/useResize.tsx
    Co-authored-by: Julia Ortiz <94128293+julia-script@users.noreply.github.com>

--- apps/mission/src/components/useResize.tsx
+++ apps/mission/src/components/useResize.tsx
@@ -7,16 +7,17 @@ function useWindowResize() {
   const [windowSize, setWindowSize] = useState({
     width: 0,
   });
-
-  const handleResize = () => {
+  // it may be interesting to use `useLayoutEffect` here
+  // as it run before the page is painted, it might help avoiding some layout flashing before this loads
+  useLayoutEffect(() => {
+    // checking for window here is unnecessary, window is never undefined inside useEffect
     setWindowSize({ width: window.innerWidth });
-  };
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
+    
+    // If it's not being used elsewhere, prefer creating this inside the useEffect so you don't need to pass it as a dependency of useEffect
+    // I wonder why eslint  didn't complain about that btw
+   const handleResize = () => {
       setWindowSize({ width: window.innerWidth });
-    }
-
+    };
     window.addEventListener("resize", handleResize);
 
     return () => window.removeEventListener("resize", handleResize);

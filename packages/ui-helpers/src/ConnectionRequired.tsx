5d3c73cdaf5b6218f161223aa042e4a3727a1cf4
Renzo <renzobarrionuevo@gmail.com>
2024-05-13 09:24:56 -0300

    rounded widget

--- packages/ui-helpers/src/ConnectionRequired.tsx
+++ packages/ui-helpers/src/ConnectionRequired.tsx
@@ -15,21 +15,23 @@ export const ConnectionRequired = ({
 }) => {
   const { t } = useTranslation("dappStore");
   return (
-    <div
-      className={`relative blur-image after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:z-10 after:bg-[rgba(0,0,0,.3)] z-[8] h-[450px] bg-center bg-no-repeat bg-cover ${bgUrl} flex flex-col justify-center`}
-    >
-      <div className="z-[9999] text-center flex flex-col items-center space-y-6 px-8">
-        <IconUser />
-        <div className="space-y-2">
-          <h6 className="text-heading dark:text-heading-dark text-xl">
-            {t("instantdApp.signRequired")}
-          </h6>
-          <p className="text-subheading dark:text-subheading-dark text-sm">
-            {t("instantdApp.singInToInteract")}
-            {dappName} {t("instantdApp.IdApp")}
-          </p>
+    <div className="rounded-3xl overflow-hidden">
+      <div
+        className={`relative blur-image after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:z-10 after:bg-[rgba(0,0,0,.3)] z-[8] h-[450px] bg-center bg-no-repeat bg-cover ${bgUrl} flex flex-col justify-center`}
+      >
+        <div className="z-[9999] text-center flex flex-col items-center space-y-6 px-8">
+          <IconUser />
+          <div className="space-y-2">
+            <h6 className="text-heading dark:text-heading-dark text-xl">
+              {t("instantdApp.signRequired")}
+            </h6>
+            <p className="text-subheading dark:text-subheading-dark text-sm">
+              {t("instantdApp.singInToInteract")}
+              {dappName} {t("instantdApp.IdApp")}
+            </p>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     </div>
   );

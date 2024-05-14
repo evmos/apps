c1b8fdcb0489cf2911d79645934ce76f14b726ba
Renzo <renzobarrionuevo@gmail.com>
2024-05-14 09:19:38 -0300

    fix favorites

--- apps/mission/src/components/Sidebar.tsx
+++ apps/mission/src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
-
+"use client";
 import { IconHome3 } from "@evmosapps/ui/icons/line/basic/home-3.tsx";
 import { IconPlanet } from "@evmosapps/ui/icons/line/basic/planet.tsx";
 import { IconWrench } from "@evmosapps/ui/icons/line/basic/wrench.tsx";
@@ -26,53 +26,77 @@ import { IconMedium } from "@evmosapps/ui/icons/social/medium.tsx";
 import { FavoriteSection } from "./FavoriteSection";
 
 import { TrackerEvent } from "@evmosapps/ui-helpers";
-import { CLICK_ON_FOOTER_CTA, CLICK_ON_NAVIGATION } from "tracker";
+import { CLICK_ON_FOOTER_CTA } from "tracker";
+import { useState } from "react";
 
-const NavigationSection = () => (
-  <nav>
-    <h2 className="text-xs px-4 hidden md:block">dApp Store</h2>
-    <ul className="flex md:flex-col md:gap-y-2">
-      {[
-        {
-          label: "Discovery",
-          Icon: IconHome3,
-          active: true,
-          href: "/",
-        },
-        { label: "Categories", Icon: IconPlanet, href: "/dapps" },
-        { label: "Develop", Icon: IconWrench, href: "#" },
-        { label: "Learn", Icon: IconBook, href: DOCS_EVMOS_URL },
-      ].map(({ label, Icon, active, href }) => (
-        <TrackerEvent
-          key={label}
-          event={CLICK_ON_NAVIGATION}
-          properties={{ navigation: label }}
-        >
-          <li>
+const NavigationSection = () => {
+  const [favoritesVisible, setFavoritesVisible] = useState(false);
+
+  const toggleFavorites = () => {
+    setFavoritesVisible((prev) => !prev);
+  };
+
+  return (
+    <nav>
+      <h2 className="text-xs px-4 hidden md:block">dApp Store</h2>
+      <ul className="flex md:flex-col md:gap-y-2">
+        {[
+          {
+            label: "Discovery",
+            Icon: IconHome3,
+            active: true,
+            href: "/",
+          },
+          { label: "Categories", Icon: IconPlanet, href: "/dapps" },
+          {
+            label: "Develop",
+            Icon: IconWrench,
+            href: "#",
+          },
+          { label: "Learn", Icon: IconBook, href: DOCS_EVMOS_URL },
+          { label: "Favorites", onClick: toggleFavorites },
+        ].map(({ label, Icon, active, href, onClick }) => (
+          <li
+            key={label}
+            className={cn(
+              label === "Develop"
+                ? "hidden md:block"
+                : label === "Favorites"
+                  ? "block md:hidden"
+                  : "block",
+            )}
+          >
             <Link
-              href={href}
+              href={href || ""}
               className={cn(
                 "flex hover:bg-primary/10 border-transparent dark:hover:bg-primary-dark/10 h-11 px-4 justify-center items-center text-base rounded-t-lg",
                 "border-b-2 -mb-px md:mb-0 gap-x-2",
-                "md:rounded-full md:border-none md:justify-start ",
+                "md:rounded-full md:border-none md:justify-start",
                 {
                   "border-primary dark:border-primary-container-dark text-primary dark:text-primary-dark":
-                    active,
+                    active || favoritesVisible,
                 },
               )}
+              onClick={onClick}
             >
-              <Icon className="hidden md:inline-flex w-5 h-5" />
+              {Icon && <Icon className="hidden md:inline-flex w-5 h-5" />}
               {label}
-              {!href.startsWith("/") && (
+              {href && !href.startsWith("/") && (
                 <IconExternalLink className="hidden md:inline-flex ml-auto h-4 w-4 opacity-60" />
               )}
             </Link>
           </li>
-        </TrackerEvent>
-      ))}
-    </ul>
-  </nav>
-);
+        ))}
+      </ul>
+      {favoritesVisible && (
+        <div className="md:hidden">
+          <FavoriteSection />
+        </div>
+      )}
+    </nav>
+  );
+};
+
 const socials = [
   {
     name: "X",

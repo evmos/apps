b249d2168186c9c162bba41ecc0a6ed5a42556ed
Julia Ortiz <contato@juliaortiz.com.br>
2024-06-10 17:04:32 -0300

    starts implementing release script

--- packages/scripts/release/index.ts
+++ packages/scripts/release/index.ts
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 // Load environment variables
-import "../utils/load-env";
+// import "../utils/load-env";
 import { program } from "@commander-js/extra-typings";
 
 // load commands
--- packages/scripts/release/release.ts
+++ packages/scripts/release/release.ts
@@ -1,8 +1,185 @@
 import { program } from "@commander-js/extra-typings";
-import { } from "helpers/src/clients/github";
+import chalk from "chalk";
+import $ from "dax-sh";
+import { simpleGit, CleanOptions } from "simple-git";
+import { Log } from "helpers/src/logger";
+import getReleasePlan from "@changesets/get-release-plan";
+import { REPO_ROOT } from "../utils/constants";
+import { inspect } from "util";
+import memoize from "lodash-es/memoize";
+import { Octokit } from "octokit";
+
+const githubToken = await $`gh auth token`.text();
+export const github = new Octokit({
+  auth: githubToken,
+});
+
+const logger = Log("release");
+const getProjectReleasePlan = memoize(async (project: string) => {
+  const releasePlan = await getReleasePlan(REPO_ROOT);
+
+  const projectRelease = releasePlan.releases.find(
+    (release) => release.name === project,
+  );
+  if (!projectRelease) {
+    logger.error(
+      [
+        "",
+        chalk.red.bold(
+          `Error: Project \`${project}\` not found in release plan`,
+        ),
+        "",
+        "There seems to be no changeset that affects the project or it's dependencies",
+        "",
+        "Possible reasons:",
+        "- The project name is incorrect",
+        `- There are no changesets added to \`${project}\``,
+        `- There are no changesets added to a dependency of \`${project}\``,
+        `- dependencies marked as devDependency do not bump \`${project}\` version`,
+        `- global dependencies (root dependencies for example) do not bump \`${project}\` version`,
+      ].join("\n"),
+    );
+    process.exit(1);
+  }
+  return { ...releasePlan, projectRelease };
+});
+const getSummary = async (project: string) => {
+  const releasePlan = await getProjectReleasePlan(project);
+
+  const changesetMap = releasePlan.changesets.reduce((acc, changeset) => {
+    acc[changeset.id] = changeset;
+    return acc;
+  }, {});
+
+  const changes = releasePlan.releases.flatMap(
+    ({ name, newVersion, type, changesets }) => [
+      `${type.toLocaleUpperCase()} ${name}@${newVersion}`,
+      ...changesets.map((id) => {
+        const changeset = changesetMap[id];
+        return `- ${changeset.summary}`;
+      }),
+      "",
+    ],
+  );
+
+  return changes.join("\n");
+};
+
+const getReleaseBranchName = async (project: string) => {
+  const releasePlan = await getProjectReleasePlan(project);
+  const version = releasePlan.projectRelease.newVersion;
+
+  return `release/${project}/${version}`;
+};
+
+const git = simpleGit();
+const assertNoUncommittedChanges = async () => {
+  const status = await git.status();
+  if (status.files.length > 0) {
+    logger.error(
+      [
+        "",
+        chalk.red.bold("Error: Uncommitted changes detected"),
+        "",
+        "Please commit or stash your changes before proceeding",
+        "",
+        inspect(status.files, { colors: true, depth: 10 }),
+      ].join("\n"),
+    );
+    process.exit(1);
+  }
+};
+
+const createReleaseBranch = async (project: string) => {
+  const branch = await getReleaseBranchName(project);
+  await assertNoUncommittedChanges();
+  await git.checkoutBranch(branch, "julia/changeset");
+  // await git.checkoutBranch(branch, "origin/main");
+};
+
+const fetchMainBranch = async () => {
+  return await git.fetch("origin", "main");
+};
+
+const consumeChangesets = async () => {
+  await $`pnpm changeset version`;
+};
+
+const commitRelease = async (project: string) => {
+  const releasePlan = await getProjectReleasePlan(project);
+  const version = releasePlan.projectRelease.newVersion;
+  await git.add(".");
+  await git.commit(`chore: release ${project}@${version}`, ["--no-verify"]);
+};
+
+const pushReleaseBranch = async (project: string) => {
+  const branch = await getReleaseBranchName(project);
+  await git.push(["-u", "origin", branch, "-f"]);
+};
+const createReleasePr = async (project: string, baseBranch: string) => {
+  const releasePlan = await getProjectReleasePlan(project);
+  const summary = await getSummary(project);
+  const version = releasePlan.projectRelease.newVersion;
+
+  // console.log(`gh pr create --title "release ${project}: ${version}" --body "${summary}" --base ${baseBranch} --head $(git rev-parse --abbrev-ref head) --dry-run`)
+  // const resp =
+  //  await $`gh pr create --title "release ${project}: ${version}" --body "${summary}" --base ${baseBranch} --head $(git rev-parse --abbrev-ref head) --dry-run`
+  //     .text();
+  // console.log(resp);
+  return await github.rest.pulls.create({
+    owner: "evmos",
+    repo: "apps",
+    title: `Release ${project}: ${version}`,
+    body: summary,
+    base: baseBranch,
+    head: await getReleaseBranchName(project),
+  });
+};
+const getCurrentBranch = async () => {
+  const status = await git.status();
+  return status.current ?? "main";
+};
+
+const deleteReleaseBranch = async (project: string) => {
+  const branch = await getReleaseBranchName(project);
+  await git.deleteLocalBranch(branch, true);
+};
+
 program
   .argument("<project>")
   // .description("")
-  .action(async () => {
-    // console.log("options", project);
+  .action(async (project) => {
+    const currentBranch = await getCurrentBranch();
+    try {
+      logger.info(`Fetching origin/main`);
+      await fetchMainBranch();
+
+      logger.info(`Creating release branch for ${project}`);
+      await createReleaseBranch(project);
+
+      logger.info(`Consuming changesets`);
+      await consumeChangesets();
+
+      logger.info(`Committing release for ${project}`);
+      await commitRelease(project);
+
+      logger.info(`Pushing release branch`);
+      await pushReleaseBranch(project);
+
+      logger.info(`Creating release PRs`);
+      const mainPr = await createReleasePr(project, "main");
+      console.log(mainPr);
+      // logger.info(` - main: ${mainPr.data.url}`);
+
+      const prodPr = await createReleasePr(project, "prod");
+      // logger.info(` - prod: ${prodPr.data.url}`);
+    } catch (e) {
+      logger.error(e);
+    } finally {
+      logger.info(`Switching back to ${currentBranch}`);
+      await git.checkout(currentBranch);
+
+      logger.info(`Cleaning up release branch`);
+      await deleteReleaseBranch(project);
+    }
   });
